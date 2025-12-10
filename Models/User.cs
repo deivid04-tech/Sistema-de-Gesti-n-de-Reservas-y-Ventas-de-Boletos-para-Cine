cd7bd3cfda338e4a3a4b0fc90b0b6ac71b870356
deivid04-tech <deivid.t2002@gmail.com>
2025-12-09 23:19:40 -0400

    Add User model with properties and validations

---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CinemaApi.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required] public string Name { get; set; }
+        [Required] public string Email { get; set; }
+        [Required] public string PasswordHash { get; set; }
+        public string Role { get; set; } = "User"; // or "Admin"
+    }
+}
