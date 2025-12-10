703955dce10ec3141fc7df9d25d790353e6a8643
deivid04-tech <deivid.t2002@gmail.com>
2025-12-09 23:23:09 -0400

    Create LoginDto.cs

---
+++ DTOs/LoginDto.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CinemaApi.DTOs
+{
+    public class LoginDto
+    {
+        [Required][EmailAddress] public string Email { get; set; }
+        [Required] public string Password { get; set; }
+    }
+}
