b4f750d7aca857fe81c35357791e8b6af2a9bc7c
deivid04-tech <deivid.t2002@gmail.com>
2025-12-09 23:20:13 -0400

    Create Movie.cs

---
+++ Models/Movie.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CinemaApi.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        [Required] public string Title { get; set; }
+        public string Description { get; set; }
+        public int DurationMinutes { get; set; }
+        public bool Active { get; set; } = true;
+    }
+}
