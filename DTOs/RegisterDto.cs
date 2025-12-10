0c789a4120268768b0005a3377070e2f20eb4f56
deivid04-tech <deivid.t2002@gmail.com>
2025-12-09 23:22:46 -0400

    Create RegisterDto.cs

---
+++ DTOs/RegisterDto.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CinemaApi.DTOs
+{
+    public class RegisterDto
+    {
+        [Required] public string Name { get; set; }
+        [Required][EmailAddress] public string Email { get; set; }
+        [Required] public string Password { get; set; }
+    }
+}
